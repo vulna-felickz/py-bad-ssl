123f438246ff9fa55b0708f5899c44a84f7b8324
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-02-04 10:26:00 -0500

    explicitly allow sslv2 and v3

--- bad_ssl_example.py
+++ bad_ssl_example.py
@@ -2,8 +2,13 @@ import ssl
 import urllib.request
 
 def create_insecure_ssl_context():
-    # BAD: Using an old version of SSL and not doing certificate checking
-    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    context = ssl.SSLContext(ssl.PROTOCOL_SSLv23) #This is an alias to PROTOCOL_TLS - which when coupled with SSLContext will by default disable SSLv2/v3 (see https://docs.python.org/3/library/ssl.html#ssl.SSLContext)
+
+    # Ensure that options to disable SSLv2 and SSLv3 are not set (default behavior)
+    context.options &= ~ssl.OP_NO_SSLv2
+    context.options &= ~ssl.OP_NO_SSLv3
+
+    
     context.verify_mode = ssl.CERT_NONE
     context.check_hostname = False
     return context
